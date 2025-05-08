9a1fe3e543c9158f53025ab6425d72ea459f82d6
AdeptEagle <kerbi2600@gmail.com>
2025-05-09 04:38:07 +0800

    adjusted for online deployment

--- build.sh
+++ build.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
+
+# Install dependencies
 npm install --legacy-peer-deps
-npm install -g @angular/cli
-ng build --configuration=production --output-path=dist/angular-signup-verification-boilerplate 
+
+# Install Angular CLI
+npm install @angular/cli@17.3.2 --save-dev
+
+# Create the dist directory if it doesn't exist
+mkdir -p dist/angular-signup-verification-boilerplate
+
+# Run the build using the local Angular CLI
+./node_modules/.bin/ng build --configuration production --output-path=dist/angular-signup-verification-boilerplate
+
+# List the contents of the dist directory to verify the build
+ls -la dist/angular-signup-verification-boilerplate 
